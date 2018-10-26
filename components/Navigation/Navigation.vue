4e80efe15382d43874d26704da93284905e4f8a7
Madyan Eka <madyan.eka95@gmail.com>
2018-10-26 09:30:19 +0700

    create basic navbar

---
+++ components/Navigation/Navigation.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav
+    class="navbar is-warning"
+    role="navigation"
+    aria-label="main navigation">
+    <div
+      class="navbar-brand">
+      <nuxt-link
+        to="/"
+        class="navbar-item">Home</nuxt-link>
+    </div>
+
+    <a
+      role="button"
+      class="navbar-burger"
+      aria-label="menu"
+      aria-expanded="false">
+      <span aria-hidden="true"/>
+      <span aria-hidden="true"/>
+      <span aria-hidden="true"/>
+    </a>
+  </nav>
+</template>
