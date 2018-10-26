56fd04685d2c2c074216547b19da66026d07681f
Madyan Eka <madyan.eka95@gmail.com>
2018-10-26 09:57:41 +0700

    place navbar inside container

--- components/Navigation/Navigation.vue
+++ components/Navigation/Navigation.vue
@@ -3,32 +3,32 @@
     class="navbar is-warning"
     role="navigation"
     aria-label="main navigation">
-    <div
-      class="navbar-brand">
-      <nuxt-link
-        to="/"
-        class="navbar-item">Home</nuxt-link>
-
-      <a
-        role="button"
-        class="navbar-burger"
-        aria-label="menu"
-        aria-expanded="false">
-        <span aria-hidden="true"/>
-        <span aria-hidden="true"/>
-        <span aria-hidden="true"/>
-      </a>
-    </div>
-
-    <div
-      class="navbar-menu">
+    <div class="container">
       <div
-        class="navbar-start">
+        class="navbar-brand">
         <nuxt-link
-          to="/about"
-          class="navbar-item">
-          About
-        </nuxt-link>
+          to="/"
+          class="navbar-item">Home</nuxt-link>
+
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false">
+          <span aria-hidden="true"/>
+          <span aria-hidden="true"/>
+          <span aria-hidden="true"/>
+        </a>
+      </div>
+
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <nuxt-link
+            to="/about"
+            class="navbar-item">
+            About
+          </nuxt-link>
+        </div>
       </div>
     </div>
   </nav>
