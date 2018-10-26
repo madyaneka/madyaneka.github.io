83d346d61d892fc6e273caa012c9db2f428643ac
Madyan Eka <madyan.eka95@gmail.com>
2018-10-26 13:52:06 +0700

    create post preview

---
+++ components/Post/PostPreview.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="section">
+    <div class="columns">
+      <div class="column is-8 is-offset-2">
+        <div class="card">
+          <header class="card-header">
+            <div class="card-header-title">Title</div>
+          </header>
+          <div class="card-content">Akulah Arjunaaaaaaaa!!!</div>
+          <footer class="card-footer">
+            <a class="card-footer-item">Read more...</a>
+          </footer>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <p>blog</p>
-  </div>
+  <app-post-preview/>
 </template>
+
+<script>
+import PostPreview from '~/components/Post/PostPreview'
+
+export default {
+  components: {
+    appPostPreview: PostPreview
+  }
+}
+</script>
