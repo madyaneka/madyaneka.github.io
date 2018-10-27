927d1f58e77f5bb7f116e6b8e2bac3dbbd50c659
Madyan Eka <madyan.eka95@gmail.com>
2018-10-27 08:03:48 +0700

    fill / with my profile

--- pages/index.vue
+++ pages/index.vue
@@ -1,61 +1,81 @@
 <template>
-  <div>
-    <br><br><br><br><br>
-    <div class="columns has-text-centered">
-      <div class="column is-6 is-offset-3">
-        <logo/>
-        <h1 class="title">
-          blog
-        </h1>
-        <h2 class="subtitle">
-          My epic Nuxt.js project
-        </h2>
-        <div class="links">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            class="button--green">Documentation</a>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            class="button--grey">GitHub</a>
+  <div class="columns">
+    <div class="column">
+      <section class="hero is-danger">
+        <div class="container">
+          <div class="hero-body">
+            <h1 class="title">Madyan Eka Septian</h1>
+            <h2 class="subtitle">Kediri, East Java, Indonesia</h2>
+          </div>
         </div>
-      </div>
+      </section>
+      <section class="hero is-warning">
+        <div class="container">
+          <div class="hero-body">
+            <div class="columns">
+              <div class="column">
+                <div class="card">
+                  <div class="card-content">
+                    <div class="content has-text-centered">
+                      <p class="title">CSS</p>
+                      <hr>
+                      <ul
+                        class="is-marginless heading"
+                        style="list-style: none">
+                        <li>Bulma</li>
+                        <li>Bootstrap</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="column">
+                <div class="card">
+                  <div class="card-content">
+                    <div class="content has-text-centered">
+                      <p class="title">JavaScript</p>
+                      <hr>
+                      <ul
+                        class="is-marginless heading"
+                        style="list-style: none">
+                        <li>Vue</li>
+                        <li>Vue Resource</li>
+                        <li>Vue Router</li>
+                        <li>Vuex</li>
+                        <li>Vuepress</li>
+                        <li>Nuxt</li>
+                        <li>Axios</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="column">
+                <div class="card">
+                  <div class="card-content">
+                    <div class="content has-text-centered">
+                      <p class="title">Python</p>
+                      <hr>
+                      <ul
+                        class="is-marginless heading"
+                        style="list-style: none">
+                        <li>Odoo</li>
+                        <li>Django</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style>
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style scoped>
+.card {
+  height: 100%;
 }
 </style>
