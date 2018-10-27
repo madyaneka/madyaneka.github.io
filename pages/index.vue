ccd16af0868118986ebe08ea543e75139b1c98be
Madyan Eka <madyan.eka95@gmail.com>
2018-10-27 09:33:03 +0700

    add work experience section + changing document structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="columns">
     <div class="column">
-      <section class="hero is-danger">
+      <section class="hero is-warning">
         <div class="container">
           <div class="hero-body">
             <h1 class="title">Madyan Eka Septian</h1>
@@ -9,6 +9,7 @@
           </div>
         </div>
       </section>
+      <hr>
       <section class="hero is-warning">
         <div class="container">
           <div class="hero-body">
@@ -64,6 +65,53 @@
                       </ul>
                     </div>
                   </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <hr>
+      <section class="hero is-warning">
+        <div class="hero-body">
+          <div class="columns">
+            <div class="column is-half">
+              <p class="heading">Work Experience</p>
+              <div class="card">
+                <header class="card-header">
+                  <p class="card-header-title">Work Place</p>
+                  <a
+                    class="card-header-icon"
+                    aria-label="more options">
+                    <span class="icon">
+                      <i
+                        class="fas fa-angle-down"
+                        aria-hidden="true"/>
+                    </span>
+                  </a>
+                </header>
+                <div class="card-content">
+                  <p>Job Descriptions</p>
+                </div>
+              </div>
+            </div>
+            <div class="column is-half">
+              <p class="heading">Work Experience</p>
+              <div class="card">
+                <header class="card-header">
+                  <p class="card-header-title">Work Place</p>
+                  <a
+                    class="card-header-icon"
+                    aria-label="more options">
+                    <span class="icon">
+                      <i
+                        class="fas fa-angle-down"
+                        aria-hidden="true"/>
+                    </span>
+                  </a>
+                </header>
+                <div class="card-content">
+                  <p>Job Descriptions</p>
                 </div>
               </div>
             </div>
